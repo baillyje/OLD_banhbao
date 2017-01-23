42c7ca4befcac855f43a8f871dbc8ae539348d3f
J-p Bailly <jeanpierre.bailly@kubity.com>
2017-01-23 22:43:20 +0100

    WIP - créaction de classe de gestion de level

---
+++ Assets/Script/Land/Edge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge {
+
+	private Vector2 point1; 
+	private Vector2 point2;
+
+	private bool isCameraBlocker;
+
+	private bool isExit;
+	private Level levelDestination;
+	private Vector2 levelDestinationCoordinate;
+	private Vector2 heroDestinationCoordinate;
+
+}
---
+++ Assets/Script/Land/Level.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class Level {
+
+	private Array<Edge> edges;
+	private Array<Tile> tiles;
+	private Vector2 cameraDefaultPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
