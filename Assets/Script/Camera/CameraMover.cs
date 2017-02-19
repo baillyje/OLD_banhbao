44d81a0d8329f1bdea3d1e81f3b9930b58f16703
J-p Bailly <baillyje@gmail.com>
2017-02-19 16:13:31 +0100

    Just few things to test (add magical number to limit camera moving). I still have to understand few things about camera position and camera size.

--- Assets/Script/Camera/CameraMover.cs
+++ Assets/Script/Camera/CameraMover.cs
@@ -29,18 +29,21 @@ public class CameraMover : MonoBehaviour {
 		Vector3 worldPoint = GetComponent<Camera>().WorldToViewportPoint(target.position);
 		Vector3 localPoint = new Vector3 (target.position.x, target.position.y, target.position.z);
 
-		if (localPoint.x < Screen.width / 200)
-			localPoint.x = Screen.width / 200;
+		Debug.Log (localPoint.ToString());
+		Debug.Log (Screen.width + "x" + Screen.height);
 
+		if (localPoint.x < 12.8f)
+			localPoint.x = 12.8f;
+
+		if (localPoint.y < 9.5f)
+			localPoint.y = 9.5f;
+		/*
 		if (localPoint.y < Screen.height / 200)
 			localPoint.y = Screen.height / 200;
 
 		if (localPoint.y < Screen.height / 200)
 			localPoint.y = Screen.height / 200;
-
-		if (localPoint.y < Screen.height / 200)
-			localPoint.y = Screen.height / 200;
-
+		*/
 		Vector3 delta = localPoint - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, worldPoint.z)); //(new Vector3(0.5, 0.5, point.z));
 		Vector3 destination = transform.position + delta;
 		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
--- Assets/Script/Player/PlayerGenerator.cs
+++ Assets/Script/Player/PlayerGenerator.cs
@@ -6,11 +6,17 @@ public class PlayerGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+		Spawn (new Vector2(20, 50));
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void Spawn (Vector2 pos) {
+		this.transform.position = new Vector3 (pos.x, pos.y, 0);
 	}
 }
